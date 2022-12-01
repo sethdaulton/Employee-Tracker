19be2f8ad9b4926088c6db2d1cccb53d695f374d
Seth Daulton <seth.daulton@gmail.com>
2022-12-01 11:34:59 -0600

    intial commit

---
+++ seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (001, "Joe", "Peters", 001, 001),
+       (002, "Sam", "Fox", 002, 001),
+       (003, "Julie", "Allen", 003, 001),
+       (004. "Rick", "Martinez", 004, 001),
+       (005, "Greg", "Burak", 005, 001);
